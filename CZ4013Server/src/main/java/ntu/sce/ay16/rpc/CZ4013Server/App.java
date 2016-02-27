b29c2a4ecf1541021174e327608c39080355cae2
Ruan Pingcheng <ruanpingcheng@gmail.com>
2016-02-27 16:11:47 +0800

    Initial commit with maven configuration finished

---
+++ CZ4013Server/src/main/java/ntu/sce/ay16/rpc/CZ4013Server/App.java
@@ -0,0 +1,18 @@
+package ntu.sce.ay16.rpc.CZ4013Server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static Logger logger = LogManager.getLogger(App.class.getName());    
+
+    public static void main( String[] args )
+    {
+    		
+    }
+}
