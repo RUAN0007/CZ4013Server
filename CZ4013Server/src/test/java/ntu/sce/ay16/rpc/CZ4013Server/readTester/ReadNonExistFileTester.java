202ee756d5ef7b66952317af8e1deb75ccff1513
Ruan Pingcheng <ruanpingcheng@gmail.com>
2016-02-28 18:20:41 +0800

    Test passed

---
+++ CZ4013Server/src/test/java/ntu/sce/ay16/rpc/CZ4013Server/readTester/ReadNonExistFileTester.java
@@ -0,0 +1,89 @@
+package ntu.sce.ay16.rpc.CZ4013Server.readTester;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ntu.sce.ay16.rpc.CZ4013Server.Server;
+import ntu.sce.ay16.rpc.CZ4013Server.Util;
+
+public class ReadNonExistFileTester {
+	private Thread serverThread = null;
+	private File file = null;
+	private String filePath = "test/fake.txt";
+	private int port = 8887;
+	
+	
+	@Before
+	public void setUp() throws IOException, InterruptedException{
+		this.serverThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				new Server(port).start();				
+			}
+		});
+		this.serverThread.start();
+		
+		Thread.sleep(2000);
+		//Create the test file
+		this.file = Paths.get(filePath).toFile();
+		if(this.file.exists()) file.delete();
+		
+
+	}
+
+
+	@Test
+	public void test() throws Exception {
+
+		Map<String,Object> p = new HashMap<String,Object>();
+		p.put("time",System.currentTimeMillis());
+		p.put("code", 1);
+		p.put("path", filePath);
+
+		byte[] b = Util.marshal(p);
+
+		DatagramSocket dgs = new DatagramSocket();
+		InetAddress serverAddr = InetAddress.getLocalHost();
+		DatagramPacket request = 
+				new DatagramPacket(b, b.length, serverAddr, port);
+		dgs.send(request);
+
+
+		System.out.println("Send to server: " + p);
+
+		byte[] buffer = new byte[1024];
+		DatagramPacket reply = 
+				new DatagramPacket(buffer,buffer.length);
+		dgs.receive(reply);
+		byte[] data = Arrays.copyOf(reply.getData(), reply.getLength());
+		
+		Map<String,Object> response = Util.unmarshal(data);
+		
+		assertTrue((Integer) response.get("status") == 0);
+		assertTrue(response.get("message") instanceof String);
+		
+	}
+
+	@After
+	public void tearDown(){
+		Paths.get("test/read.txt").toFile().delete();
+		this.serverThread.interrupt();
+	}
+
+}
